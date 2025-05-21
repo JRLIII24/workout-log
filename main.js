0d5944e232446b2fb3d6019db19523f10f79d979
JRLIII24 <ralphyluis44@gmail.com>
2025-05-20 19:44:36 -0700

    First Variables in main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Workout Log script loaded!");
+
+let exerciseName = "Squats";
+let sets = 3;
+let reps = 10;
+let durationInMinutes = 5;
+
+console.log(`The exercise is: ${exerciseName}, you are going to do ${sets} sets of ${reps} reps for a duration of ${durationInMinutes} minutes`)
+let isExerciseDone = false;
+console.log("Is it done?", isExerciseDone); 
+
+isExerciseDone = true;
+console.log("Is it done now?", isExerciseDone);
+
