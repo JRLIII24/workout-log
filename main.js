17d30d494f3227a3e3c9ce21fe4e1889b370399a
JRLIII24 <ralphyluis44@gmail.com>
2025-05-21 11:59:55 -0700

    Display Div Content using query selector

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 console.log("Workout Log script loaded!");
+const displayDiv = document.querySelector("#current-exercise-display");
 
 let exerciseName = "Squats";
+
 let sets = 3;
 let reps = 10;
 let durationInMinutes = 5;
@@ -12,3 +14,4 @@ console.log("Is it done?", isExerciseDone);
 isExerciseDone = true;
 console.log("Is it done now?", isExerciseDone);
 
+displayDiv.textContent = `The exercise is: ${exerciseName}, you are going to do ${sets} sets of ${reps} reps for a duration of ${durationInMinutes} minutes`
