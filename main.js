23167c85ea9a21697934b2ddabfdefe800f5b215
JRLIII24 <ralphyluis44@gmail.com>
2025-05-22 23:31:59 -0700

    worked on creating a function to display text instead of hardcoding it

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ console.log("Workout Log script loaded!");
 const displayDiv = document.querySelector("#current-exercise-display");
 
 let exerciseName = "Squats";
-
 let sets = 3;
 let reps = 10;
 let durationInMinutes = 5;
 
-console.log(`The exercise is: ${exerciseName}, you are going to do ${sets} sets of ${reps} reps for a duration of ${durationInMinutes} minutes`)
-let isExerciseDone = false;
-console.log("Is it done?", isExerciseDone); 
+// console.log(`The exercise is: ${exerciseName}, you are going to do ${sets} sets of ${reps} reps for a duration of ${durationInMinutes} minutes`)
+// let isExerciseDone = false;
+// console.log("Is it done?", isExerciseDone); 
+// isExerciseDone = true;
+// console.log("Is it done now?", isExerciseDone);
 
-isExerciseDone = true;
-console.log("Is it done now?", isExerciseDone);
 
-displayDiv.textContent = `The exercise is: ${exerciseName}, you are going to do ${sets} sets of ${reps} reps for a duration of ${durationInMinutes} minutes`
+function displayExercise(name, sets, reps, duration) {
+    displayDiv.textContent = `The exercise is: ${name}, you are going to do ${sets} sets of ${reps} reps for a duration of ${duration} minutes`
+}
+displayExercise(exerciseName, sets, reps, durationInMinutes)
